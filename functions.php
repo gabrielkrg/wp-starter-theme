f2a8ac684d2954e594ae70d43dc778b449ab6778
Gabriel Rodrigues <gabrielkrodrigues1@gmail.com>
2022-12-15 10:20:30 -0300

    remove function to disable guttenberg

--- functions.php
+++ functions.php
@@ -38,20 +38,6 @@ function search_filter($query)
 }
 add_filter('pre_get_posts', 'search_filter');
 
-
-/**
- * Desativa Guttenberg
- */
-function prefix_disable_gutenberg($current_status, $post_type)
-{
-    // Use your post type key instead of 'product'
-    //if ($post_type !== 'post') return false;
-    //return $current_status;
-    return false;
-}
-add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
-
-
 /**
  * Remove h2 do pagination
  */
@@ -89,7 +75,7 @@ function wpdocs_filter_wp_title($title, $sep)
 add_filter('wp_title', 'wpdocs_filter_wp_title', 10, 2);
 
 /**
- * Altera quantidad de caracteres do excerpt
+ * Altera quantidade de caracteres do excerpt
  */
 function wpdocs_custom_excerpt_length($length)
 {
@@ -105,10 +91,3 @@ function new_excerpt_more($more)
     return '...';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
-
-
-// add_action('admin_init', 'cmfc_posts_order');
-// function cmfc_posts_order()
-// {
-//     add_post_type_support('post', 'page-attributes');
-// }
