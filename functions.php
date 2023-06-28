d42f3c09e975c00323b19cda000d2b8b49c2c972
Gabriel <gabrielkrodrigues1@gmail.com>
2023-06-28 20:06:40 -0300

    remove functions

--- functions.php
+++ functions.php
@@ -29,16 +29,6 @@ add_image_size('default', 768, 330, true);
 
 
 /**
- * Remove acentos no Namespace
- */
-add_filter('sanitize_file_name', 'sa_sanitize_spanish_chars', 10);
-function sa_sanitize_spanish_chars($filename)
-{
-    return remove_accents($filename);
-}
-
-
-/**
  * Filtro de busca
  */
 add_filter('pre_get_posts', 'search_filter');
@@ -50,42 +40,6 @@ function search_filter($query)
         }
         return $query;
     }
-}
-
-/**
- * Remove h2 do pagination
- */
-add_action('navigation_markup_template', 'sanitize_pagination');
-function sanitize_pagination($content)
-{
-    $content = preg_replace('#<h2.*?>(.*?)<\/h2>#si', '', $content);
-    return $content;
-}
-
-/**
- * Altera a função wp_title()
- */
-add_filter('wp_title', 'wpdocs_filter_wp_title', 10, 2);
-function wpdocs_filter_wp_title($title, $sep)
-{
-    global $paged, $page;
-
-    if (is_feed())
-        return $title;
-
-    // Add the site name.
-    $title .= get_bloginfo('name');
-
-    // Add the site description for the home/front page.
-    $site_description = get_bloginfo('description', 'display');
-    if ($site_description && (is_home() || is_front_page()))
-        $title = "$title $sep $site_description";
-
-    // Add a page number if necessary.
-    if ($paged >= 2 || $page >= 2)
-        $title = "$title $sep " . sprintf(__('Page %s', 'twentytwelve'), max($paged, $page));
-
-    return $title;
 }
 
 /**
