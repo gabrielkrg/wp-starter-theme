7227002b1ee5d9b25f53226c1f0eed89bbf4a7dd
Gabriel <gabrielkrodrigues1@gmail.com>
2024-01-21 11:28:17 -0300

    update

--- page.php
+++ page.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
-<section id="default">
+<section id="page" class="py-4">
     <div class="container">
-        <div class="row mb-5">
+        <div class="row">
             <div class="col-12">
+                <h1>
+                    <?php the_title(); ?>
+                </h1>
                 <?php the_content(); ?>
             </div>
         </div>
