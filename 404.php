131d8b789aa1b009025f23a108860d38f3b24f1b
Gabriel <gabrielkrodrigues1@gmail.com>
2024-01-21 13:15:10 -0300

    404

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
-<section id="not-found">
+<section id="not-found" class="py-4">
     <div class="container">
         <div class="row mb-5">
             <div class="col-12">
-                
+                <h1>Página não encontrada!</h1>
+                <p>404</p>
             </div>
         </div>
     </div>
